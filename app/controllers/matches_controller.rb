5024b8cc8739065596eb81ac76af714b2a8b77c1
George Pogrebnoi <gosha1416@yandex.ru>
2023-04-24 14:32:59 +0300

    Fix and Refactor

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -17,37 +17,9 @@ class MatchesController < ApplicationController
   end
 
   def create
-    @match = Match.new(match_params)
-    data = DotaApi::API.parce_match(@match.serial)
-
-    @match.score_radiant = data['dire_score']
-    @match.score_dire = data['radiant_score']
-
-    if @match.save
-      data['players'].each do |player|
-        match_stat = @match.match_stats.create()
-        match_stat.kills = player['kills']
-        match_stat.deaths = player['deaths']
-        match_stat.assists = player['assists']
-        match_stat.last_hits = player['last_hits']
-        match_stat.denies = player['denies']
-        match_stat.networce = player['net_worth']
-        match_stat.networce = player['net_worth']
-        match_stat.hero = Hero.find_by(hero_id: player['hero_id'])
-
-        if Account.exists?(steamID32: player['account_id'])
-          match_stat.account = Account.find_by(steamID32: player['account_id'])
-        elsif player['account_id']
-          account = Account.new(steamID32: player['account_id'])
-          account_data = DotaApi::API.parce_player(player['account_id'])
-          account.username = account_data['personaname']
-          account.save
-          match_stat.account = account
-        end
-
-        match_stat.save
-      end
-
+    puts match_params
+    @match = create_match_by_serial(match_params[:serial])
+    if @match
       redirect_to @match
     else
       render :new, status: :unprocessable_entity
@@ -77,6 +49,46 @@ class MatchesController < ApplicationController
 
   private
   def match_params
-    params.require(:match).permit(:serial, :score_radiant, :score_dire)
+    # params.require(:match).permit(:serial, :score_radiant, :score_dire)
+    params.require(:match).permit(:serial)
+  end
+
+  def create_match_by_serial(serial)
+    match = Match.new(match_params)
+    data = DotaApi::API.parce_match(match.serial)
+
+    match.score_radiant = data['dire_score']
+    match.score_dire = data['radiant_score']
+
+    if match.save
+      data['players'].each do |player|
+        match_stat = match.match_stats.create()
+        match_stat.kills = player['kills']
+        match_stat.deaths = player['deaths']
+        match_stat.assists = player['assists']
+        match_stat.last_hits = player['last_hits']
+        match_stat.denies = player['denies']
+        match_stat.networce = player['net_worth']
+        match_stat.networce = player['net_worth']
+        match_stat.hero = Hero.find_by(hero_id: player['hero_id'])
+
+        if Account.exists?(steamID32: player['account_id'])
+          match_stat.account = Account.find_by(steamID32: player['account_id'])
+        elsif player['account_id']
+          account = Account.new(steamID32: player['account_id'])
+          account_data = DotaApi::API.parce_player(player['account_id'])
+          if account_data['personaname']
+            account.username = account_data['personaname']
+          end
+          account.save
+          match_stat.account = account
+        end
+
+        match_stat.save
+      end
+      return match
+    end
+
+    return nil
   end
 end
