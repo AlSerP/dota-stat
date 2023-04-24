310caf77b079d8343e8b63e9ab66bcac88ac810d
George Pogrebnoi <gosha1416@yandex.ru>
2023-04-24 14:32:18 +0300

    Field uniquness

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ApplicationRecord
     has_many :match_stats
+    validates :steamID32, uniqueness: true
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,6 @@
 class Match < ApplicationRecord
     has_many :match_stats, dependent: :destroy
-    validates :serial, presence: true
+    validates :serial, presence: true, uniqueness: true
     validates :score_radiant, presence: true
     validates :score_dire, presence: true
 end
